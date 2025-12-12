0ad71e9593a3a3ad0cd4e4a113eeeef8351ee3cb
WilliamRo <walienluo@gmail.com>
2025-12-12 17:54:55 +0800

    [M] fixed get_main_file_dir not working in debug mode issue

--- talos/utils/file_manager.py
+++ talos/utils/file_manager.py
@@ -1,13 +1,20 @@
+import sys
 import os
-import traceback
 
 
 def get_main_file_dir() -> str:
-  """Get the main file directory from the traceback.
+  """Get the main file directory of the currently running script.
 
   Returns:
       str: The main file directory.
   """
-  main_file_path = traceback.extract_stack()[0].filename
-  main_file_dir = os.path.dirname(main_file_path)
-  return main_file_dir
+  # Get the module object for the entry point
+  main_module = sys.modules['__main__']
+
+  # Check if it has a file attribute (it might not in a REPL/shell)
+  if hasattr(main_module, '__file__'):
+    main_file_path = os.path.abspath(main_module.__file__)
+    return os.path.dirname(main_file_path)
+
+  print(" ! Cannot determine main file path in interactive mode.")
+  return None
